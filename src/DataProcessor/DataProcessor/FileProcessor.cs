4d3b1b7c2f43c2a087242eb083f87bf870ec2f07
jakob-hendrix <jhendrix001@gmail.com>
2019-05-07 07:26:06 -0500

    File found!

---
+++ src/DataProcessor/DataProcessor/FileProcessor.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using System.IO;
+
+namespace DataProcessor
+{
+    /// <summary>
+    /// Process a file
+    /// </summary>
+    internal class FileProcessor
+    {
+        public string InputFilePath;
+
+        public FileProcessor(string filePath)
+        {
+            this.InputFilePath = filePath;
+        }
+
+        public void Process()
+        {
+            WriteLine($"Begin processing of {InputFilePath}");
+
+            // check if file exists
+            if (!File.Exists(InputFilePath))
+            {
+                WriteLine($"ERROR: file {InputFilePath} not found.");
+                return;
+            }
+        }
+    }
+}
--- src/DataProcessor/DataProcessor/Program.cs
+++ src/DataProcessor/DataProcessor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace DataProcessor
 {
@@ -6,7 +7,40 @@ namespace DataProcessor
     {
         static void Main(string[] args)
         {
+            var command = args[0];
 
+            if (command == "--file")
+            {
+                var filePath = args[1];
+                WriteLine($"Single file {filePath} selected");
+                ProcessSingleFile(filePath);
+            }
+            else if (command == "--dir")
+            {
+                var directoryPath = args[1];
+                var fileType = args[2];
+                WriteLine($"Directory {directoryPath} selected for {fileType} files");
+                ProcessDirectory(directoryPath, fileType);
+            }
+            else
+            {
+                WriteLine("Invalid command line options");
+            }
+
+            WriteLine("Press enter to quit");
+            ReadLine();
+        }
+
+
+
+        private static void ProcessSingleFile(string filePath)
+        {
+            var fileProcessor = new FileProcessor(filePath);
+            fileProcessor.Process();
+        }
+        private static void ProcessDirectory(string directoryPath, string fileType)
+        {
+            throw new NotImplementedException();
         }
     }
 }
